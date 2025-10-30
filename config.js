f6b22ad19c6812b6ce1f4c7a962e63a6535c0d5a
xandergozali28-cmd <xandergozali28@gmail.com>
2025-10-30 20:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://xandergozali:gozali2228@@discordbot.f1llzan.mongodb.net/?",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
